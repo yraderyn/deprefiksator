a9925a4522025a78b26586e694fc7539919b395e
Stefan Ivanović <s.ivanovic@outlook.com>
2019-03-14 00:02:54 +0100

    Dodao pravljenje posebne liste koja će obuhvatiti sve reči

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 from prefiksi import prefiksator
 
 if __name__ == '__main__':
@@ -16,6 +16,7 @@ if __name__ == '__main__':
 		lista_sufiksa = ['evljanin', 'ijacija', 'ionizam', 'ionista', 'ovnjača', 'ovština', 'evština', 'ajlija', 'anstvo', 'ancija', 'aonica', 'arijum', 'arnica', 'atizam', 'ašnica', 'ekanja', 'encija', 'enjača', 'eskara', 'edžija', 'ijanac', 'ikovac', 'instvo', 'ionist', 'ionica', 'istika', 'jetica', 'ljanin', 'obanja', 'ovište', 'ovnica', 'otinja', 'turača', 'turina', 'uljaga', 'uljača', 'uljina', 'uljica', 'urdija', 'urlija', 'uskara', 'uskera', 'ušnica', 'uština', 'čurina', 'avina', 'avica', 'avnik', 'agija', 'adija', 'ajica', 'alija', 'alica', 'aljka', 'ander', 'anija', 'janin', 'anica', 'arija', 'arina', 'arica', 'arnik', 'atika', 'atura', 'ahija', 'acija', 'ačija', 'benik', 'elica', 'eljak', 'endra', 'enica', 'enjak', 'erina', 'esija', 'esina', 'etika', 'etina', 'ešina', 'ijada', 'ijana', 'ijera', 'ilije', 'ilica', 'iljka', 'inica', 'injak', 'injac', 'ioner', 'ionaš', 'išnik', 'etica', 'kelja', 'kinja', 'kovac', 'lište', 'njava', 'njača', 'ovača', 'evača', 'ovilo', 'ovina', 'evina', 'ovica', 'evica', 'ovlje', 'evlje', 'ovnik', 'olija', 'olina', 'olica', 'oljak', 'onica', 'onjak', 'orija', 'osija', 'otina', 'otica', 'udija', 'uljak', 'unica', 'urija', 'urina', 'usina', 'ušina', 'ušica', 'čanin', 'džija', 'ština', 'avac', 'avka', 'adak', 'adba', 'azan', 'ajka', 'ajko', 'alac', 'alje', 'anac', 'anik', 'anin', 'anka', 'ance', 'anče', 'anja', 'arak', 'arac', 'arak', 'arij', 'arka', 'aroš', 'ater', 'ator', 'aćka', 'ačka', 'ašin', 'aška', 'ašce', 'bina', 'vica', 'dura', 'ejac', 'elac', 'elin', 'elja', 'enac', 'enik', 'ence', 'eraj', 'erak', 'erac', 'erda', 'etak', 'etin', 'ečak', 'ešce', 'ivač', 'idba', 'izam', 'ijan', 'ijat', 'ijer', 'ilac', 'ilja', 'inac', 'inka', 'inče', 'inja', 'inje', 'ista', 'itak', 'itet', 'itis', 'ićak', 'ičar', 'ičić', 'ična', 'išan', 'iška', 'ište', 'jeha', 'kost', 'lama', 'leta', 'lija', 'lica', 'ljag', 'ljaj', 'nina', 'nica', 'nost', 'njak', 'ovac', 'evac', 'ovik', 'ović', 'ević', 'ovka', 'ojka', 'ujko', 'olan', 'olet', 'onik', 'onja', 'otak', 'ošta', 'stvo', 'telj', 'ulja', 'unac', 'unče', 'urak', 'urda', 'utak', 'utin', 'juca', 'udža', 'ušak', 'ušar', 'ušac', 'ušić', 'uška', 'cija', 'čaga', 'čija', 'čina', 'čica', 'čuga', 'čura', 'džik', 'štak', 'ava', 'aga', 'ada', 'aža', 'aik', 'aja', 'aje', 'aka', 'alo', 'alj', 'ana', 'and', 'ant', 'ara', 'aća', 'ača', 'eza', 'elj', 'ent', 'esa', 'est', 'eta', 'eto', 'eut', 'eša', 'ivo', 'ija', 'ije', 'ika', 'ilo', 'ina', 'ing', 'ino', 'ist', 'ica', 'iša', 'jak', 'eha', 'juh', 'kan', 'lac', 'luk', 'nik', 'nja', 'nje', 'oba', 'ovo', 'evo', 'uza', 'oje', 'ost', 'ota', 'oća', 'oša', 'tak', 'tva', 'tor', 'uga', 'uža', 'ulj', 'ura', 'uca', 'uša', 'čak', 'čić', 'dža', 'av', 'ag', 'ad', 'aj', 'ak', 'an', 'en', 'ao', 'ar', 'at', 'ać', 'ac', 'ač', 'aš', 'ba', 'va', 'ež', 'ez', 'en', 'er', 'et', 'eš', 'id', 'ik', 'im', 'in', 'ir', 'it', 'ić', 'uć', 'ic', 'iš', 'ja', 'je', 'jo', 'uh', 'ka', 'ki', 'ko', 'la', 'le', 'lo', 'mo', 'no', 'ov', 'on', 'or', 'os', 'ot', 'oč', 'oš', 'st', 'ta', 'ća', 'će', 'ug', 'un', 'ur', 'us', 'ut', 'uh', 'uš', 'ca', 'ce', 'ča', 'če', 'ša', 'a', 'e', 'o']
 		lista_infinitiva = ['karati', 'uckati', 'uljiti', 'ušiti', 'arati', 'avati', 'evati', 'ivati', 'ovati', 'isati', 'irati', 'ijati', 'nuti', 'kati', 'ati', 'iti', 'eti', 'ti']
 		lj_lista = ['blj', 'vlj', 'plj', 'mlj']
+		lista_fajlova = ['uslov_0.txt', 'uslov_1.txt', 'uslov_2.txt', 'uslov_3.txt', 'uslov_4.txt', 'uslov_5.txt', 'uslov_6.txt']
 
 
 		# leme glagola
@@ -29,6 +30,13 @@ if __name__ == '__main__':
 
 		for reč in rečnik:
 			prefiksator(reč, lista_prefiksa, rečnik, lista_sufiksa, lista_infinitiva, lista_glagola, lj_lista)
+
+		with open('lista_svih.txt', 'a+') as izlaz:
+			for fajl in lista_fajlova:
+				with open(fajl) as ulaz:
+					for red in ulaz:
+						red = re.sub(' .*', '', red)
+						izlaz.write(red)
 
 	else:
 		"Došlo je do greške, pokrenite program ponovo."
